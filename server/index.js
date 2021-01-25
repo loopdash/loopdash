1e9d1782790c38ce5a8f5ae0cbe623a10f3be324
Gary Bunofsky <gary@garybunofsky.com>
2021-01-24 22:04:55 -0500

    Separated client and server

---
+++ server/index.js
@@ -0,0 +1,80 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const axios = require("axios");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = require("path");
+const _ = require("lodash");
+
+const MAILGUN_KEY = process.env.MAILGUN_KEY;
+const MAILGUN_DOMAIN = process.env.MAILGUN_DOMAIN;
+const UPTIME_ROBOT_KEY = process.env.UPTIME_ROBOT_KEY;
+const mailgun = require("mailgun-js")({
+  apiKey: MAILGUN_KEY,
+  domain: MAILGUN_DOMAIN
+});
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.set("PORT", process.env.PORT || 8080);
+
+app.use(express.static(path.resolve(__dirname, "../client/dist")));
+
+app.post("/api/send-email", (req, res) => {
+  const { email } = req.body;
+
+  if (!email) {
+    return res.status(400).json({
+      status: "error",
+      body: "Missing required fields: title or author"
+    });
+  }
+
+  const data = {
+    from: "Loopdash lead form <admin@loopdash.com>",
+    to: "gary@garybunofsky.com",
+    subject: "New lead",
+    text: `A potential customer has filled out the form: ${email}`
+  };
+
+  mailgun.messages().send(data, (error, body) => {
+    console.log(body);
+  });
+
+  return res.status(200).json({
+    status: "success",
+    body: `Queued email to ${email}`
+  });
+});
+
+app.get("/api/get-server-status", (req, res) => {
+  axios
+    .post("https://api.uptimerobot.com/v2/getMonitors", {
+      api_key: `${UPTIME_ROBOT_KEY}`,
+      all_time_uptime_ratio: 1
+    })
+    .then(response =>
+      res.status(200).json({
+        status: "success",
+        data: {
+          averageUptime: _.meanBy(response.data.monitors, m =>
+            parseInt(m.all_time_uptime_ratio)
+          ),
+          isDown: _.some(response.data.monitors, { status: 9 })
+        }
+      })
+    )
+    .catch(error =>
+      res.status(500).json({
+        status: "failed",
+        data: error
+      })
+    );
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`App is running on ${process.env.PORT}`);
+});
