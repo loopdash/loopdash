c5b21821bc0da50b43656b4e8c5b1bec46a6a815
Gary Bunofsky <gary@garybunofsky.com>
2021-01-31 22:05:17 -0500

    Deleted everything

---
+++ .nuxt/components/index.js
@@ -0,0 +1,35 @@
+export { default as CallToAction } from '../../components/global/CallToAction.vue'
+export { default as Footer } from '../../components/global/Footer.vue'
+export { default as GlobalNav } from '../../components/global/GlobalNav.vue'
+export { default as Hero } from '../../components/global/Hero.vue'
+export { default as Banner } from '../../components/Banner.vue'
+export { default as Features } from '../../components/Features.vue'
+export { default as Form } from '../../components/Form.vue'
+export { default as HostingQuestions } from '../../components/HostingQuestions.vue'
+export { default as ProConList } from '../../components/ProConList.vue'
+export { default as Question } from '../../components/Question.vue'
+export { default as Services } from '../../components/Services.vue'
+export { default as TeamMember } from '../../components/TeamMember.vue'
+export { default as TeamMembers } from '../../components/TeamMembers.vue'
+export { default as Testimonial } from '../../components/Testimonial.vue'
+export { default as Testimonials } from '../../components/Testimonials.vue'
+export { default as TextSet } from '../../components/TextSet.vue'
+export { default as WebsiteQuestions } from '../../components/WebsiteQuestions.vue'
+
+export const LazyCallToAction = import('../../components/global/CallToAction.vue' /* webpackChunkName: "components/global/CallToAction" */).then(c => c.default || c)
+export const LazyFooter = import('../../components/global/Footer.vue' /* webpackChunkName: "components/global/Footer" */).then(c => c.default || c)
+export const LazyGlobalNav = import('../../components/global/GlobalNav.vue' /* webpackChunkName: "components/global/GlobalNav" */).then(c => c.default || c)
+export const LazyHero = import('../../components/global/Hero.vue' /* webpackChunkName: "components/global/Hero" */).then(c => c.default || c)
+export const LazyBanner = import('../../components/Banner.vue' /* webpackChunkName: "components/Banner" */).then(c => c.default || c)
+export const LazyFeatures = import('../../components/Features.vue' /* webpackChunkName: "components/Features" */).then(c => c.default || c)
+export const LazyForm = import('../../components/Form.vue' /* webpackChunkName: "components/Form" */).then(c => c.default || c)
+export const LazyHostingQuestions = import('../../components/HostingQuestions.vue' /* webpackChunkName: "components/HostingQuestions" */).then(c => c.default || c)
+export const LazyProConList = import('../../components/ProConList.vue' /* webpackChunkName: "components/ProConList" */).then(c => c.default || c)
+export const LazyQuestion = import('../../components/Question.vue' /* webpackChunkName: "components/Question" */).then(c => c.default || c)
+export const LazyServices = import('../../components/Services.vue' /* webpackChunkName: "components/Services" */).then(c => c.default || c)
+export const LazyTeamMember = import('../../components/TeamMember.vue' /* webpackChunkName: "components/TeamMember" */).then(c => c.default || c)
+export const LazyTeamMembers = import('../../components/TeamMembers.vue' /* webpackChunkName: "components/TeamMembers" */).then(c => c.default || c)
+export const LazyTestimonial = import('../../components/Testimonial.vue' /* webpackChunkName: "components/Testimonial" */).then(c => c.default || c)
+export const LazyTestimonials = import('../../components/Testimonials.vue' /* webpackChunkName: "components/Testimonials" */).then(c => c.default || c)
+export const LazyTextSet = import('../../components/TextSet.vue' /* webpackChunkName: "components/TextSet" */).then(c => c.default || c)
+export const LazyWebsiteQuestions = import('../../components/WebsiteQuestions.vue' /* webpackChunkName: "components/WebsiteQuestions" */).then(c => c.default || c)
