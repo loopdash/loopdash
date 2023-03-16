e825b34bbf8049cea1a570429edbc137cf95faa2
Gary Bunofsky <gary@garybunofsky.com>
2023-03-16 08:12:06 -0400

    Added robots.txt

--- .eleventy.js
+++ .eleventy.js
@@ -43,6 +43,7 @@ module.exports = function (eleventyConfig) {
   });
 
   eleventyConfig.addPassthroughCopy('src/_redirects');
+  eleventyConfig.addPassthroughCopy({ 'src/robots.txt': '/robots.txt' });
 
   return {
     dir: {
