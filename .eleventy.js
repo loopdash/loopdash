f12b07223e971f6f819a2fe770929b599a96e3f9
Gary Bunofsky <gary@garybunofsky.com>
2024-11-23 22:50:32 -0500

    Finally fixed

---
+++ .eleventy.js
@@ -0,0 +1,104 @@
+import sitemap from "@quasibit/eleventy-plugin-sitemap";
+import { minify as htmlmin } from "html-minifier";
+import { inspect } from "util";
+import eleventyImg from "@11ty/eleventy-img";
+import dotenv from "dotenv";
+
+// Load environment variables
+dotenv.config();
+
+export default function (eleventyConfig) {
+  // Image shortcode
+  eleventyConfig.addNunjucksAsyncShortcode(
+    "image",
+    async (src, alt, widths = [300, 600, null], formats = ["webp", "jpeg"]) => {
+      if (!alt) {
+        throw new Error(`Missing \`alt\` attribute for image: ${src}`);
+      }
+
+      let metadata = await eleventyImg(src, {
+        widths: widths,
+        formats: formats,
+        outputDir: "dist/images", // Adjusted to use "dist"
+        urlPath: "/images",
+      });
+
+      let imageAttributes = {
+        alt,
+        sizes: "(max-width: 600px) 100vw, 600px",
+        loading: "lazy",
+        decoding: "async",
+      };
+
+      return eleventyImg.generateHTML(metadata, imageAttributes);
+    }
+  );
+
+  // Environment variables filter
+  eleventyConfig.addFilter("env", (key) => process.env[key]);
+
+  // Debug filter
+  eleventyConfig.addFilter("debug", (content) => `<pre>${inspect(content)}</pre>`);
+
+  // Category filter
+  eleventyConfig.addFilter("categoryFilter", function (collection, category) {
+    if (!category) return collection;
+    return collection.filter((item) => item.data.category === category);
+  });
+
+  // Limit filter
+  eleventyConfig.addNunjucksFilter("limit", (arr, limit) => arr.slice(0, limit));
+
+  // Template formats
+  eleventyConfig.setTemplateFormats([
+    "md",
+    "css",
+    "njk",
+    "js",
+    "jpg",
+    "jpeg",
+    "png",
+    "webp",
+    "gif",
+    "svg",
+    "eot",
+    "ttf",
+    "woff",
+    "woff2",
+    "ico",
+    "mp4",
+  ]);
+
+  // Sitemap plugin
+  eleventyConfig.addPlugin(sitemap, {
+    sitemap: {
+      hostname: process.env["ROOT_URL"],
+    },
+  });
+
+  // HTML minification
+  eleventyConfig.addTransform("htmlmin", (content, outputPath) => {
+    if (outputPath && outputPath.endsWith(".html")) {
+      return htmlmin(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true,
+      });
+    }
+    return content;
+  });
+
+  // Passthrough copy
+  eleventyConfig.addPassthroughCopy("src/_redirects");
+  eleventyConfig.addPassthroughCopy({ "src/robots.txt": "/robots.txt" });
+
+  return {
+    dir: {
+      input: "src",
+      includes: "_includes",
+      layouts: "_includes/layouts",
+      output: "_site",
+    },
+    passthroughFileCopy: true,
+  };
+}
