ce1556109319ecd4da96054a0815f625f2e54f6b
Gary Bunofsky <gary@garybunofsky.com>
2025-02-16 22:25:31 -0500

    combined normalize and base.css

--- .eleventy.js
+++ .eleventy.js
@@ -198,9 +198,10 @@ function renderForm(res, errorMessage) {
     <html>
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
-        <link rel="stylesheet" href="../../css/normalize.css"/>
         <link rel="stylesheet" href="../../css/base.css"/>
         <script src="../../javascript/scripts.min.js" defer></script>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
       </head>
       <body>
         <main>
